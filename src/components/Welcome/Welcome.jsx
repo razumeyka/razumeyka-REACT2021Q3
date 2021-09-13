9970785895df42080317665c52a4a803e7c1e359
razumeyka <33911265+razumeyka@users.noreply.github.com>
2021-09-13 17:08:25 +0300

    responsive and texts added

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -8,7 +8,7 @@ const Welcome = () => (
     <main className={classes['main-screen']}>
         <Wrapper>
             <h1 className={classes.heading}>Practice React with RSS-school</h1>
-            <h2 className={classes.subheading}>This website is my studying project for learning React. During development I trained such themes: components, hooks, forms, API, routing. In navigation you can see tasks in details. If you would like to see the code, there is a link to the repository - <a href="hhh">Some link</a></h2>
+            <h2 className={classes.subheading}>This website is my studying project for learning React. During development I have created webpack config and used features of React: JSX, components, hooks, forms, API, routing. In navigation you can see some tasks in details. If you would like to see the code, there is a link to the repository - <a href="https://github.com/razumeyka/razumeyka-REACT2021Q3" target="_blank" rel="noreferrer">Github link</a></h2>
         </Wrapper>
     </main>
 );
